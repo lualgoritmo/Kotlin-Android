1888aa4d2375791e3e824653f266c4295c5f8a42
Luciano Garcia <lualgoritmo@outlook.com>
2022-04-12 18:52:35 -0300

    Alterei o exercicio 3

--- IntroducaoKotlin/src/Exercicio3.kt
+++ IntroducaoKotlin/src/Exercicio3.kt
@@ -1,15 +1,15 @@
 fun main() {
-    println("Digite o número");
-    var num: Int = readln().toInt();
-    var multiplo: Int = 5;
-    var resultado: Int = num * multiplo;
-    println("É multiplo de 5: $num , resultado: $resultado");
 
-    /*var x: Int = 0;
-    when (x) {
-        in 1..10 -> print("x 1")
-        in 10..20 -> print("x 2")
-        else -> print("3")
-    }*/
+    println("Digite o multiplicador")
+    val num: Int = readln().toInt();
+    println("Digite até quanto")
+    var n: Int = readln().toInt();
+    for (i in 0..n step num) {
+        if(i % num == 0){
+            println("\n Multiplicador: $num\n Resultado: $i")
+        }
+    }
 }
+
+
 
