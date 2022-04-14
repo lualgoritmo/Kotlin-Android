51d761047aedd809730bda452722cf5ba4bf8863
Luciano Garcia <lualgoritmo@outlook.com>
2022-04-13 22:42:30 -0300

    Faendo

--- Funcao/src/media.kt
+++ Funcao/src/media.kt
@@ -4,11 +4,13 @@ fun main() {
     media(5f,10f,2.5f,10f);
     media("","Luana", 1,2,3,4,);
 }
+//VARARGS PARA RECEBER TODOS OS TIPOS
 fun <T> media(vararg valores:T){
     for(valor in valores){
         println(valor);
     }
 }
+//VARARGS PARA RECEBER UMAS LISTA,ARRAY DE UM TIPO
 /*fun media(vararg notas:Float){
     if(notas.isNotEmpty()){
         var soma = 0f;
