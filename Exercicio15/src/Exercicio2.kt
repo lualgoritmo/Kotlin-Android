0a14c7cb44fa2893fd0eb9222d5a70eefc160c9d
Luciano Garcia <lualgoritmo@outlook.com>
2022-04-24 17:06:05 -0300

    Falta finalizar o exércicio 4

--- Exercicio15/src/Exercicio2.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    println("Digite um número");
-    var n:Int = readln().toInt();
-    var numero = IntArray(9);
-    for (i in n..numero.size) {
-        var soma = i * 2;
-        println(soma);
-    }
-}
